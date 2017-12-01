d09284d96ea8b4d660363d765f2ccc16c99af696
HYmian <gopher.mian@outlook.com>
2017-12-01 17:28:56 +0800

    fix: get deploy components from cluster instead of playbook folder

--- playbook/deploy_seed.go
+++ playbook/deploy_seed.go
@@ -9,6 +9,30 @@ import (
 
 type DeploySeed2 map[string]*Component
 
+func NewDeploySeed(c *database.Cluster, workDir string) map[string]*Component {
+	cs := make(map[string]*Component)
+	for _, cp := range c.Components {
+		hosts := make(map[string][]*database.Host)
+		for k, v := range cp.Hosts {
+			hs := make([]*database.Host, 0, len(v))
+			for _, h := range v {
+				host := getEntireHost(c, h)
+				hs = append(hs, host)
+			}
+
+			hosts[k] = hs
+		}
+
+		cs[cp.Name] = &Component{
+			MetaComponent: cp.MetaComponent,
+			Hosts:         hosts,
+			Inherent:      getInherentProperties(path.Join(workDir, cp.Name+PlaybookSuffix, cp.Version)),
+		}
+	}
+
+	return cs
+}
+
 func (ds *DeploySeed2) AllHosts() []*database.Host {
 	hosts := make([]*database.Host, 0)
 	for _, v := range map[string]*Component(*ds) {
@@ -20,82 +44,12 @@ func (ds *DeploySeed2) AllHosts() []*database.Host {
 	return hosts
 }
 
-// func (d *DeploySeed2) EsEndpoint() string {
-// 	if esVip, find := d.LoadBalancer.Property["es_vip"]; find {
-// 		return esVip.(string)
-// 	}
-
-// 	if len(d.K8sNode.Hosts) > 0 {
-// 		return d.K8sNode.Hosts[0].IP
-// 	}
-
-// 	return ""
-// }
-
-// func (d *DeploySeed2) OtherEndpoint() string {
-// 	if otherVip, find := d.LoadBalancer.Property["other_vip"]; find {
-// 		return otherVip.(string)
-// 	}
-
-// 	if len(d.K8sNode.Hosts) > 0 {
-// 		return d.K8sNode.Hosts[0].IP
-// 	}
-
-// 	return "192.168.0.1"
-// }
-
-// func (d *DeploySeed2) RegistryEndpoint() string {
-// 	if registryVip, find := d.LoadBalancer.Property["registry_vip"]; find {
-// 		return registryVip.(string)
-// 	}
-
-// 	if len(d.Registry.Hosts) > 0 {
-// 		return d.Registry.Hosts[0].IP
-// 	}
-
-// 	return ""
-// }
-
-// func (d *DeploySeed2) MySQLEndpoint() string {
-// 	if mysqlVip, find := d.LoadBalancer.Property["mysql_vip"]; find {
-// 		return mysqlVip.(string)
-// 	}
-
-// 	if len(d.MySQL.Hosts) > 0 {
-// 		return d.MySQL.Hosts[0].IP
-// 	}
-
-// 	return ""
-// }
-
-// func (d *DeploySeed2) K8sEndpoint() string {
-// 	if k8sVip, find := d.LoadBalancer.Property["k8s_vip"]; find {
-// 		return k8sVip.(string)
-// 	}
-
-// 	if len(d.K8sMaster.Hosts) > 0 {
-// 		return d.K8sMaster.Hosts[0].IP
-// 	}
-
-// 	return ""
-// }
-
 type Component struct {
 	database.MetaComponent
 	Inherent map[string]interface{}
 	Hosts    map[string][]*database.Host
 }
 
-func NewDeploySeed(c *database.Cluster, workDir string) *DeploySeed2 {
-	ds := DeploySeed2(make(map[string]*Component))
-
-	for _, cp := range GetComponents(workDir) {
-		component := getComponent(c, cp, workDir)
-		ds[cp] = component
-	}
-
-	return &ds
-}
 
 func setComponentHost(
 	clusterID string,
@@ -132,30 +86,6 @@ func ConvertHosts(
 	}
 
 	return nil
-}
-
-func getComponent(c *database.Cluster, componentName string, workDir string) *Component {
-	for _, cp := range c.Components {
-		if componentName == cp.Name {
-			hosts := make(map[string][]*database.Host)
-			for k, v := range cp.Hosts {
-				hs := make([]*database.Host, 0, len(v))
-				for _, h := range v {
-					host := getEntireHost(c, h)
-					hs = append(hs, host)
-				}
-
-				hosts[k] = hs
-			}
-			return &Component{
-				MetaComponent: cp.MetaComponent,
-				Hosts:         hosts,
-				Inherent:      getInherentProperties(path.Join(workDir, cp.Name+PlaybookSuffix, cp.Version)),
-			}
-		}
-	}
-
-	panic(fmt.Sprintf("can't find the component: %s", componentName))
 }
 
 func getEntireHost(c *database.Cluster, host string) *database.Host {
